6c890934445cf5f83d0f9191fd874b0ab4901380
DarioKnezovic <dknezovic997@gmail.com>
2023-11-08 19:15:50 +0000

    Created migration for modal_ctr_tracking table.

---
+++ pkg/database/migrations/002_create_modal_ctr_tracking_table.sql
@@ -0,0 +1,11 @@
+-- Migration created at 2023-11-08T19:07:21Z
+
+CREATE TABLE modal_ctr_tracking (
+                                    id INT IDENTITY(1,1) PRIMARY KEY,
+                                    user_id VARCHAR(255) NOT NULL,
+                                    timestamp TIMESTAMP NOT NULL,
+                                    interaction_type VARCHAR(50) NOT NULL,
+                                    object_id VARCHAR(255) NOT NULL,
+                                    additional_data VARCHAR(255),
+                                    campaign_id INT NOT NULL
+);
